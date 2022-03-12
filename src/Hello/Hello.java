38c86a66babd977de106ff8556b0ca1479647740
nextv-aizawa <nextv-aizawa@users.noreply.github.com>
2022-03-12 11:08:47 +0900

    Merge branch 'master' of https://github.com/nextv-iori/nextv

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!!");
-		// testaizawa20220312
+		// testaizawa20220312a
 		// kamikamikamiyamayama
 		// 20210312kamikami
 		// ioriiori
