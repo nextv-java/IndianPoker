0512890712440a7801ed6f22fef5ccb8f452ced7
kamiyan.aka.k-real <kamiyan.aka.k-real@localhost>
2022-05-14 13:00:13 +0900

    神山比較処理追加

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -4,8 +4,24 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!! world");
-
-	}
-
+		// System.out.println("Hello World!! world");
+		int playNum = 10;	//自分の番号
+		int comNum = 100;	//相手の番号
+		boolean myGuess = true;	//自分の予想
+		int WinLose = 0;	//結果
+		
+		//関数呼び出し
+		Result_HighLow result = new Result_HighLow();
+		WinLose = result.Result(playNum, comNum, myGuess);
+//		result.Result(num1, num2, WinLose);
+		
+		//結果出力（仮）
+		if(WinLose == 2) {
+			System.out.println("Your WIN!!");
+		} else if(WinLose == 0) {
+			System.out.println("Your Lose");
+		} else {
+			System.out.println("Draw");
+		}
+	}	
 }
---
+++ src/Hello/Result_HighLow.java
@@ -0,0 +1,36 @@
+package Hello;
+
+public class Result_HighLow {
+//	public static void main(String[] args) {
+//		// TODO 自動生成されたメソッド・スタブ
+//		System.out.println("Hello World!! world");
+//	}
+	
+	public int Result(int player, int com, boolean guess) {
+		int Win = 2;
+		int Lose = 0;
+		int Draw = 1;
+		
+		//値の比較
+		//プレイヤーが[High]と予想
+		if(guess) {
+			if(player > com) {
+				return Win;		//プレイヤーが勝ち
+			} else if(player < com) {
+				return Lose;	//プレイヤーが負け
+			} else {
+				return Draw;	//引き分け
+			}
+		}
+		//プレイヤーが[Low]と予想
+		else {
+			if(player < com) {
+				return Win;		//プレイヤーが勝ち
+			} else if(player > com) {
+				return Lose;	//プレイヤーが負け
+			} else {
+				return Draw;	//引き分け
+			}
+		}
+	}
+}
